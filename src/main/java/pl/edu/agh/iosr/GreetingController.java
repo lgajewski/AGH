b17d553d5ef14c9a9be5e2f958501fc5004bf6b9
Łukasz Gajewski <lukasz.gajewski94@gmail.com>
2017-10-27 00:08:33 +0200

    [distributed-systems-raft] Configure Heroku for auto deployment

--- src/main/java/pl/edu/agh/iosr/GreetingController.java
+++ src/main/java/pl/edu/agh/iosr/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
 
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping({"/", "/greeting"})
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(TEMPLATE, name));
