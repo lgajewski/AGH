0b52b49a7d02ae12f9f5f65f7b6b73b3782b7af3
Paweł Kocot <pawel.kacper.kocot@gmail.com>
2017-11-17 00:59:08 +0100

    [distributed-systems-raft] client-server communication init

--- scalanode/src/main/scala/pl/edu/agh/iosr/raft/scala/client/Client.scala
+++ scalanode/src/main/scala/pl/edu/agh/iosr/raft/scala/client/Client.scala
@@ -1,10 +1,22 @@
 package pl.edu.agh.iosr.raft.scala.client
 
-import akka.actor.{Actor, ActorLogging, ActorRef}
+import java.io.File
 
-class Client(server: ActorRef) extends Actor with ActorLogging {
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+
+class Client() extends Actor with ActorLogging {
+
+  private val server = context.actorSelection("akka.tcp://iosr-raft-main@127.0.0.1:11223/user/server")
 
   override def receive: Receive = {
-    case _  =>
+    case _ =>
   }
+}
+
+object Client extends App {
+  val configFile = getClass.getClassLoader.getResource("client.conf").getFile
+  val config = ConfigFactory.parseFile(new File(configFile))
+  val system = ActorSystem("ClientSystem", config)
+  val client = system.actorOf(Props[Client], name="client")
 }
--- scalanode/src/main/scala/pl/edu/agh/iosr/raft/scala/server/Server.scala
+++ scalanode/src/main/scala/pl/edu/agh/iosr/raft/scala/server/Server.scala
@@ -1,14 +1,12 @@
 package pl.edu.agh.iosr.raft.scala.server
 
-import akka.actor.{Actor, ActorLogging, ActorSystem}
+import akka.actor.{Actor, ActorLogging}
 import akka.routing.{ActorRefRoutee, BroadcastRoutingLogic, Router}
 
 class Server(nodeCount: Int = 5) extends Actor with ActorLogging {
 
-  final val system = ActorSystem("iosr-raft")
-
   private val nodes = Vector.tabulate(nodeCount) { i =>
-    system.actorOf(Node.props(this.self), "NODE_" + (i + 1))
+    context.actorOf(Node.props(this.self), "NODE_" + (i + 1))
   }
 
   val broadcastRouter: Router = Router(BroadcastRoutingLogic(), nodes.map(ActorRefRoutee))
